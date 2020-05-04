1fbbfd36be259c68ef15099cdf287c1ab49ac13f
Sylvia Engmann <sylvia.engmann8@gmail.com>
2020-05-04 20:29:49 +0000

    Day 1 - Data Types

---
+++ Day01-DataTypes.py
@@ -0,0 +1,24 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j = None
+k = None
+l = None
+
+# Read and save an integer, double, and String to your variables.
+j = int(input())
+k = float(input())
+l = str(input())
+
+# Print the sum of both integer variables on a new line.
+int_sum = i + j
+print(int_sum)
+# Print the sum of the double variables on a new line.
+doub_sum = d + k
+print(doub_sum)
+# Concatenate and print the String variables on a new line
+str_var = s + l
+print(str_var)
+# The 's' variable above should be printed first.
+
